2cbd0263633f0f3c08765d77dc413a9223905f2a
zhfang <zhfang@i9i8.com>
2016-12-18 18:14:53 +0800

    add music

--- usr/themes/default/footer.php
+++ usr/themes/default/footer.php
@@ -5,6 +5,9 @@
 </div><!-- end #body -->
 
 <footer id="footer" role="contentinfo">
+    <div class="music-bar" style="position: fixed;left: 0;bottom: 0;">
+        <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=27538254&auto=1&height=66"></iframe>
+    </div>
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
 </footer><!-- end #footer -->
